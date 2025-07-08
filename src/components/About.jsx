02c89cbb970d37f1a42a27ac645d659a4ada6ef1
MuFaizann <163081174+MuFaizann@users.noreply.github.com>
2025-07-08 16:09:51 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,12 +10,14 @@ const About = () => {
 
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-          Welcome! I am Faizan, I have a background in Computer Science and experience in frontend development.
-          I specialize in building clean, interactive user experiences. With a understanding of Agile methodologies
+          Welcome! I am Faizan, I have a background in Computer Science and experience in frontend development. 
+          I specialize in building clean, interactive user experiences. With an understanding of Agile methodologies
           and Software Development Life Cycles (SDLC), I bring both technical expertise and project management skills
-          to the table. I'm familiar with tools like Jira, and Slack to keep teams aligned and projects
-          moving forward. Whether I'm writing code or driving progress, I focus on clarity, collaboration,
-          and results.
+          to the table. In addition to my development background, I have knowledge in data entry and proficient with
+          Microsoft Office applications, enabling me to handle a variety of administrative and technical tasks.
+          I'm familiar with tools like Jira and Slack to keep teams aligned and projects moving forward.
+          Whether I'm writing code, managing data, or driving progress, 
+          I focus on clarity, collaboration, and results.
           </p>
         </div>
 
@@ -25,3 +27,13 @@ const About = () => {
 }
 
 export default About
+
+/*
+Welcome! I am Faizan, I have a background in Computer Science and experience in frontend development.
+          I specialize in building clean, interactive user experiences. With a understanding of Agile methodologies
+          and Software Development Life Cycles (SDLC), I bring both technical expertise and project management skills
+          to the table. I'm familiar with tools like Jira, and Slack to keep teams aligned and projects
+          moving forward. Whether I'm writing code or driving progress, I focus on clarity, collaboration,
+          and results.
+      */
+
